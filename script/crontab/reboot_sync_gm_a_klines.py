7e8db48dedfa878fe1d3d8ae6c2bafc0042b0a83
wangxu <1058715329@qq.com>
2024-04-23 13:27:19 +0800

    选股与数据同步演示代码修改

--- script/crontab/reboot_sync_gm_a_klines.py
+++ script/crontab/reboot_sync_gm_a_klines.py
@@ -9,6 +9,7 @@ from tqdm.auto import tqdm
 
 from chanlun import config, fun
 from chanlun.exchange.exchange_db import ExchangeDB
+from chanlun.exchange.exchange import convert_stock_kline_frequency
 
 """
 同步股票数据到数据库中
@@ -25,64 +26,10 @@ set_token(config.GM_TOKEN)
 
 db_ex = ExchangeDB("a")
 
-# 获取沪深 股票/基金/指数 代码
-# fund_stocks = get_instruments(symbols=None, exchanges=['SHSE', 'SZSE'],
-#                               sec_types=[SEC_TYPE_FUND])
-#
-# stock_stocks = get_instruments(symbols=None, exchanges=['SHSE', 'SZSE'],
-#                                sec_types=[SEC_TYPE_STOCK])
-# run_codes = ['SHSE.000001'] + [s['symbol'] for s in stock_stocks] + [s['symbol'] for s in fund_stocks if
-#                                                                      s['symbol'][0:7] in ['SHSE.51', 'SZSE.15']]
-
-
 symbols = get_symbols(sec_type1=1010, sec_type2=101001)
 run_codes = [_s["exchange"] + "." + _s["sec_id"] for _s in symbols]
-for _c in [
-    "SH.000001",
-    "SH.000010",
-    "SH.000016",
-    "SZ.399001",
-    "SZ.399330",
-    "SZ.399005",
-    "SH.000300",
-    "SH.000905",
-    "SH.000906",
-    "SH.000852",
-    "SZ.399006",
-    "SZ.399673",
-    "SZ.399295",
-    "SZ.399296",
-    "SZ.399362",
-    "SH.000689",
-    "SH.000015",
-    "SH.000092",
-    "SH.000919",
-    "SZ.399971",
-    "SH.000685",
-    "SZ.399967",
-    "SZ.399973",
-    "SH.000819",
-    "SH.000928",
-    "SZ.399998",
-    "SZ.399976",
-    "SZ.399808",
-    "SH.000827",
-    "SZ.399986",
-    "SZ.399975",
-    "SZ.399966",
-    "SH.000992",
-    "SH.000018",
-    "SZ.399995",
-    "SZ.399989",
-    "SH.000991",
-    "SH.000913",
-    "SH.000933",
-    "SH.000036",
-    "SH.000069",
-    "SH.000932",
-    "SZ.399987",
-]:
-    run_codes.append(_c.replace("SZ.", "SZSE.").replace("SH.", "SHSE."))
+# 增加上证指数代码
+run_codes.append("SHSE.000001")
 
 print("Sync Len : ", len(run_codes))
 
@@ -91,7 +38,7 @@ now_datetime = datetime.datetime.now()
 
 # 默认第一次同步的起始时间，后续则进行增量更新（新版掘金 分钟数据只能下载最近的 6个月数据）
 sync_frequencys = {
-    "d": {"start": "2005-01-01 00:00:00"},
+    "d": {"start": "1990-01-01 00:00:00"},
     # "30m": {
     #     "start": fun.datetime_to_str(
     #         datetime.datetime.now() - datetime.timedelta(days=180), "%Y-%m-%d"
@@ -107,41 +54,60 @@ sync_frequencys = {
 # 本地周期与掘金周期对应关系
 fre_maps = {"1m": "60s", "5m": "300s", "30m": "1800s", "d": "1d"}
 
-# K线数据采用后复权，可增量更新
-is_update = False
-for code in tqdm(run_codes):
-    # 中途终端后，可以设置下次起始的代码
-    # if not is_update and code == 'SHSE.600433':
-    #     is_update = True
-    # if not is_update:
-    #     continue
-
-    for f, dt in sync_frequencys.items():
-        try:
-            while True:
-                time.sleep(1)
-                last_dt = db_ex.query_last_datetime(code, f)
-                if last_dt is None:
+if __name__ == "__main__":
+    # K线数据采用前复权，如发现之前数据有变化，则删除并重新下载
+    for code in tqdm(run_codes, desc="同步进度"):
+        for f, dt in sync_frequencys.items():
+            try:
+                while True:
+                    time.sleep(1)
                     last_dt = dt["start"]
-                klines = history(
-                    code,
-                    fre_maps[f],
-                    start_time=last_dt,
-                    end_time=now_datetime,
-                    adjust=ADJUST_POST,
-                    df=True,
-                    fill_missing="Last",
-                )
-                klines.loc[:, "code"] = klines["symbol"]
-                klines.loc[:, "date"] = pd.to_datetime(klines["eob"])
-                klines = klines[
-                    ["code", "date", "open", "close", "high", "low", "volume"]
-                ]
-                print("Run code %s frequency %s klines len %s" % (code, f, len(klines)))
-                db_ex.insert_klines(code, f, klines)
-                if len(klines) < 500:
-                    break
+                    last_klines = db_ex.klines(code, f, args={"limit": 100})
+                    if len(last_klines) > 0:
+                        last_dt = last_klines.iloc[0]["date"]
+
+                    klines = history(
+                        code,
+                        fre_maps[f],
+                        start_time=last_dt,
+                        end_time=now_datetime,
+                        adjust=ADJUST_PREV,
+                        df=True,
+                    )
+                    klines.loc[:, "code"] = klines["symbol"]
+                    klines.loc[:, "date"] = pd.to_datetime(klines["eob"])
+                    klines = klines[
+                        ["code", "date", "open", "close", "high", "low", "volume"]
+                    ]
+                    # 如果之前数据库有记录，与新请求的数据进行对比，如果收盘价有变化，则进行重新更新
+                    if len(last_klines) > 0:
+                        if last_klines.iloc[0]["close"] != klines.iloc[0]["close"]:
+                            tqdm.write(
+                                f"Run code {code} frequency {f} 数据异常，删除重新请求"
+                            )
+                            db_ex.del_klines_by_code_freq(code, f)
+                            continue
+                    tqdm.write(
+                        f"Run code {code} frequency {f} klines len {len(klines)}"
+                    )
+                    db_ex.insert_klines(code, f, klines)
+                    if len(klines) < 500:
+                        break
+            except Exception as e:
+                tqdm.write(f"执行 {code} 同步K线异常")
+                traceback.format_exc()
+                time.sleep(10)
+
+    # TODO 掘金没有周线数据，需要自己转换
+    # 将日线数据转换成周线数据，并存储到数据库中
+    for code in tqdm(run_codes, desc="转换进度"):
+        try:
+            # 获取所有日线K线
+            klines_d = db_ex.klines(code, "d", args={"limit": 9999999})
+            klines_w = convert_stock_kline_frequency(klines_d, "w")
+            db_ex.insert_klines(code, "w", klines_w)
+            tqdm.write(f"转换 {code} 周线数据：{len(klines_w)}")
         except Exception as e:
-            print("执行 %s 同步K线异常" % code)
-            print(traceback.format_exc())
-            time.sleep(10)
+            tqdm.write(f"执行 {code} 转换数据K线异常")
+
+    print("Done")
